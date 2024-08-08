633950cb1aead67bc4d82e576b145f88ace98961
Sergey Ivanov <sivanov@unison.team>
2024-08-08 15:26:17 +0300

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -24932,10 +24932,10 @@ async function run() {
   try {
     const inputs = {
       webhookURL: core.getInput('YUCHAT_URL', { required: true }),
-      channel: core.getInput('YUCHAT_CHANNEL'),
-      workspace: core.getInput('YUCHAT_WORKSPACE'),
-      token: core.getInput('YUCHAT_TOKEN'),
-      text: core.getInput('MARKDOWN'),
+      channel: core.getInput('YUCHAT_CHANNEL', { required: true }),
+      workspace: core.getInput('YUCHAT_WORKSPACE', { required: true }),
+      token: core.getInput('YUCHAT_TOKEN', { required: true }),
+      text: core.getInput('MARKDOWN', { required: true }),
     }
 
     const finalPayload = await generatePayload(inputs)
